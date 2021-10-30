50feefaada18924a5c14ea66415fcc82c1bf5ead
Tatsat Tendulkar <78213136+tatsat1@users.noreply.github.com>
2021-10-29 22:11:49 -0400

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+/**
+ * Add your code here to fix URI connection
+ * 
+ */
+module.exports = {
+    "URI": "mongodb+srv://Tatsat:tatsat@cluster0.kqyvc.mongodb.net/BooksDB?retryWrites=true&w=majority"
+
+
+}
